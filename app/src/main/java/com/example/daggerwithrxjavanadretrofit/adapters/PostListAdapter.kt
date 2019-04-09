04ae67e7293c5fdf6d8be602e5bf45b49496e86c
Ambar Kumar <ambar.ara123@gmail.com>
2019-04-09 21:46:33 +0530

    adding file

---
+++ app/src/main/java/com/example/daggerwithrxjavanadretrofit/adapters/PostListAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.daggerwithrxjavanadretrofit.adapters
+
+class PostListAdapter {
+}
---
+++ app/src/main/java/com/example/daggerwithrxjavanadretrofit/view_model/PostViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.daggerwithrxjavanadretrofit.view_model
+
+class PostViewModel {
+}
